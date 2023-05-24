aada58b3cba9ca362fd219cc7089b2b316a974f6
Ryu0118 <87907656+Ryu0118@users.noreply.github.com>
2023-05-24 18:44:18 +0900

    first commit

---
+++ Formula/dgraph.rb
@@ -0,0 +1,34 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Sorter < Formula
+  desc "CLI tool to output mermaid dependencies between Swift Package Manager targets"
+  homepage "https://github.com/Ryu0118/swift-dependencies-graph"
+  url "https://github.com/Ryu0118/swift-dependencies-graph/releases/download/0.0.1/dgraph"
+  sha256 "5fa96909143087b855fde6ed11e710be04a71e322f9dac37686d28f2e2eb2b0f"
+  license "MIT"
+
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
+
+    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    bin.install "dgraph"
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test Kusa`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "#{bin}/dgraph", "--version"
+  end
+end
